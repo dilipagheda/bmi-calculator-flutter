1b35761b433de3ac0762474a551080c3a90a73ad
Dilip <dilip_agheda@yahoo.com>
2019-05-16 09:15:59 +1000

    Refactored and created a seperate component for RoundIconButton

---
+++ lib/RoundIconButton.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function onClick;
+  RoundIconButton({@required this.icon, @required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        child: Icon(
+          icon,
+          color: Colors.white,
+        ),
+        elevation: 6.0,
+        shape: CircleBorder(),
+        onPressed: onClick,
+        fillColor: Colors.pink,
+        constraints: BoxConstraints(
+          minWidth: 56.0,
+          minHeight: 56.0,
+        ));
+  }
+}
