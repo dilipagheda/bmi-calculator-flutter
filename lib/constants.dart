d857736ecd2739613893dd3fc59611090eff9c98
Dilip <dilip_agheda@yahoo.com>
2019-05-16 09:14:27 +1000

    Created seperate constants file

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kCardColor = Colors.black;
+const kSelectedCardColor = Colors.pink;
+
+const kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.normal,
+);
+
+const kContentStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+
+const kButtonTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 30,
+  fontWeight: FontWeight.bold,
+);
+
+const kHeaderStyle = TextStyle(
+  color: Colors.pink,
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
