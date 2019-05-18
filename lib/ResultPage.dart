7297146db09d77cdbf2ffad53f4fa2725f5e02a4
Dilip <dilip_agheda@yahoo.com>
2019-05-18 13:42:36 +1000

    Modified ResultPage to pull data from model

--- lib/ResultPage.dart
+++ lib/ResultPage.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
 import 'action_button.dart';
 import 'constants.dart';
+import 'model/BMI.dart';
 import 'reusable_card.dart';
 
 class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double bmiValue = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-            child: Text(
-              "Your Result",
-              style: kHeaderStyle,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Your Result",
+                style: kHeaderStyle,
+              ),
             ),
           ),
           Expanded(
-            flex: 5,
+            flex: 6,
             child: ReusableCard(
               cardColor: kCardColor,
               content: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    "NORMAL",
-                    style: kLabelStyle,
-                  ),
-                  Text(
-                    "22.1",
+                    BMI.interpretation(bmiValue),
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 80,
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    bmiValue.toStringAsFixed(1),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 100,
                       color: Colors.white,
                     ),
                   ),
@@ -51,14 +60,27 @@ class ResultPage extends StatelessWidget {
                   SizedBox(
                     height: 30,
                   ),
-                  Text("You have a normal BMI", style: kLabelStyle)
+                  Center(
+                    child: Text(
+                      BMI.recommendation(bmiValue),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
           ),
           ActionButton(
-            caption: "RE-CALCULATE",
-          )
+              caption: "RE-CALCULATE",
+              action: (bmi) {
+                print("pop");
+                Navigator.pop(context);
+              })
         ],
       ),
     );
